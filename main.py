e95c725afda0fff95ad635857db3cc090b3c16c0
tobi <it.hcol@proton.me>
2026-09-25 23:43:53 +0200

    add frame limiter/clock

--- main.py
+++ main.py
@@ -13,6 +13,11 @@ def main():
     while True:
         log_state()
 
+        clock = pygame.time.Clock()
+        dt = 0.0
+
+
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
@@ -21,6 +26,8 @@ def main():
 
 
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
 
 
 if __name__ == "__main__":
